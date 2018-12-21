801e97a40b7a69faef2dfd417e455168fec7ab67
Kurokoda <indigo.developer@gmail.com>
2018-12-21 16:39:45 -0500

    Improved section divider

--- src/component/divider/index.js
+++ src/component/divider/index.js
@@ -3,6 +3,7 @@
  */
 import { css, StyleSheet } from 'aphrodite';
 import React, { Component } from 'react';
+import Logo from '../../assets/images/logo/index';
 import { ApplicationContext } from '../../context/application';
 
 /**
@@ -20,15 +21,22 @@ class Divider extends Component {
   }
 
   render() {
+    const { theme } = this.context;
+    const styles = Divider.getStyles({ theme });
+
     return (
-        <div id="divider" className={css(styles.container)}>
-          <div className={css(styles.spacer)} />
-          <div className={css(styles.middle)} />
-          <div className={css([styles.spacer, styles.bottom])} />
-          <div className={css(styles.icon)}>
-            <i className="fas fa-atom"></i>
-          </div>
+      <div id="divider" className={css(styles.container)}>
+        <div className={css(styles.spacer)} />
+        <div className={css(styles.middle)} />
+        <div className={css([styles.spacer, styles.bottom])} />
+        <div className={css(styles.icon)}>
+          <Logo
+            background={theme.getIn(['app', 'color', 'background'])}
+            foreground={theme.getIn(['app', 'color', 'logo'])}
+            size={24}
+          />
         </div>
+      </div>
     );
   }
 }
@@ -36,31 +44,41 @@ class Divider extends Component {
 Divider.contextType = ApplicationContext;
 
 // TODO convert JS-in-CSS to CSS when possible
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'relative'
-  },
-  icon: {
-    top: '25%',
-    color: 'black',
-    background: 'white',
-    padding: '0 20px 0 20px',
-    position: 'absolute',
-    left: 'calc(50% - 22px)',
-    width: '44px'
-  },
-  middle: {
-    background: 'linear-gradient(to right, #ffffff 25%,#000000 51%,#ffffff 75%)',
-    height: '1px',
-  },
-  bottom: {
-    margin: '0px 0px 10px !important',
-  },
-  spacer: {
-    height: '20px',
-    width: '100%'
-  }
-});
+Divider.getStyles = config =>
+  StyleSheet.create({
+    container: {
+      position: 'relative'
+    },
+    icon: {
+      top: '0',
+      color: 'black',
+      position: 'absolute',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '43px',
+      left: '0',
+      width: '100%'
+    },
+    middle: {
+      background: `linear-gradient(to right, ${config.theme.getIn([
+        'app',
+        'color',
+        'background'
+      ])} 25%, ${config.theme.getIn([
+        'app',
+        'color',
+        'logo'
+      ])} 51%, ${config.theme.getIn(['app', 'color', 'background'])} 75%)`,
+      height: '1px'
+    },
+    bottom: {
+      margin: '0px 0px 10px !important'
+    },
+    spacer: {
+      height: '20px',
+      width: '100%'
+    }
+  });
 
 export default Divider;
